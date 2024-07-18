399f19fad0360afedce9ead43c30839a5ee957e5
simonekosa <simone_kosa@hotmail.com>
2024-07-17 23:10:06 -0300

    add cards na main page

--- lib/componentes/custom_drawer.dart
+++ lib/componentes/custom_drawer.dart
@@ -12,161 +12,173 @@ class CustomDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-          child: ListView(
-            children: [
-              const UserAccountsDrawerHeader(
-                  decoration: BoxDecoration(color: Colors.pink),                                   
-                  currentAccountPicture: CircleAvatar(                    
-                      backgroundImage: AssetImage("lib/images/userimagem.jpg")),
-                  accountName: Text("Simone Kosa"),
-                  accountEmail: Text("simone_kosa@hotmail.com")),
-              const SizedBox(
-                height: 2,
-              ),
-              const Divider(color: Colors.transparent),
-              ListTile(
-                leading: const Icon(Icons.home),
-                iconColor: Colors.pink,
-                title: const Text("Dados Cadastrais"),
-                tileColor: const Color.fromARGB(14, 233, 30, 98),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const DadosCadastraisPage()));
-                },
-              ),
-              const Divider(color: Colors.transparent),
-              ListTile(
-                leading: const Icon(Icons.home),
-                iconColor: Colors.pink,
-                title: const Text("Calcular Imc"),
-                tileColor: const Color.fromARGB(14, 233, 30, 98),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const CalcularImcPage()));
-                },
-              ),
-              const Divider(color: Colors.transparent),
-              ListTile(
-                leading: const Icon(Icons.home),
-                iconColor: Colors.pink,
-                title: const Text("Listar Resultados"),
-                tileColor: const Color.fromARGB(14, 233, 30, 98),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const PaginaEvolucao()));
-                },
-              ),
-              const Divider(color: Colors.transparent),
-              ListTile(
-                leading: const Icon(Icons.home),
-                iconColor: Colors.pink,
-                title: const Text("Listar Tarefas"),
-                tileColor: const Color.fromARGB(14, 233, 30, 98),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const ListaTarefasPage()));
-                },
-              ),
-              const Divider(color: Colors.transparent),
-              ListTile(
-                leading: const Icon(Icons.home),
-                iconColor: Colors.pink,
-                title: const Text("Configurações"),
-                tileColor: const Color.fromARGB(14, 233, 30, 98),
-                onTap: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const ConfiguracoesPage()));
-                },
-              ),
-              const Divider(color: Colors.transparent),
-              ListTile(
-                leading: const Icon(Icons.done_all),
-                iconColor: Colors.pink,
-                title: const Text("Termos de uso e privacidade"),
-                tileColor: const Color.fromARGB(14, 233, 30, 98),
-                onTap: () {
-                  showModalBottomSheet(
-                    context: context, 
-                    builder: (BuildContext bc){
-                        return SingleChildScrollView(
-                          child: Container(
-                            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 12),
-                            child: const Center(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Text("Termos de Uso e Política de Privacidade",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w700)),                            
-                                  Text("Ao utilizar nossos serviços, você concorda com os seguintes termos e condições. Coletamos informações pessoais, como nome, e-mail e dados de uso, para proporcionar  uma melhor experiência ao usuário. Seus dados são armazenados de forma segura e utilizados apenas para fins operacionais, aprimoramento dos nossos serviços e comunicação com você. Não compartilhamos suas informações com terceiros sem seu consentimento, exceto quando exigido por lei. Você tem o direito de acessar, corrigir ou solicitar a exclusão de seus dados pessoais a qualquer momento. Utilizamos cookies e tecnologias similares para melhorar a funcionalidade do nosso site e analisar o uso do serviço. Ao continuar a navegação, você aceita o uso dessas tecnologias. Reservamo-nos o direito de modificar estes termos a qualquer momento. Quaisquer alterações serão publicadas em nosso site e entrarão em vigor imediatamente. Recomendamos revisar periodicamente esta política para estar ciente de quaisquer mudanças. Se tiver dúvidas ou preocupações sobre nossa política de privacidade, entre em contato conosco.",textAlign: TextAlign.justify,),
-                                ],
-                              ),
-                            ),                           
-                          ),
-                        );
-                    }
-                    );
-                },
-              ),
-              const Divider(color: Colors.transparent),
-              ListTile(
-                leading: const Icon(Icons.exit_to_app),
-                iconColor: Colors.pink,
-                title: const Text("Sair"),
-                tileColor: const Color.fromARGB(14, 233, 30, 98),
-                onTap: () {
-                 showDialog(
-                  context: context, 
+      child: ListView(
+        children: [
+          const UserAccountsDrawerHeader(
+              decoration: BoxDecoration(color: Colors.pink),
+              currentAccountPicture: CircleAvatar(
+                  backgroundImage: AssetImage("lib/images/userimagem.jpg")),
+              accountName: Text("Simone Kosa"),
+              accountEmail: Text("simone_kosa@hotmail.com")),
+          const SizedBox(
+            height: 2,
+          ),
+          const Divider(color: Colors.transparent),
+          ListTile(
+            leading: const Icon(Icons.home),
+            iconColor: Colors.pink,
+            title: const Text("Dados Cadastrais"),
+            tileColor: const Color.fromARGB(14, 233, 30, 98),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const DadosCadastraisPage()));
+            },
+          ),
+          const Divider(color: Colors.transparent),
+          ListTile(
+            leading: const Icon(Icons.home),
+            iconColor: Colors.pink,
+            title: const Text("Calcular Imc"),
+            tileColor: const Color.fromARGB(14, 233, 30, 98),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const CalcularImcPage()));
+            },
+          ),
+          const Divider(color: Colors.transparent),
+          ListTile(
+            leading: const Icon(Icons.home),
+            iconColor: Colors.pink,
+            title: const Text("Listar Resultados"),
+            tileColor: const Color.fromARGB(14, 233, 30, 98),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const PaginaEvolucao()));
+            },
+          ),
+          const Divider(color: Colors.transparent),
+          ListTile(
+            leading: const Icon(Icons.home),
+            iconColor: Colors.pink,
+            title: const Text("Listar Tarefas"),
+            tileColor: const Color.fromARGB(14, 233, 30, 98),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ListaTarefasPage()));
+            },
+          ),
+          const Divider(color: Colors.transparent),
+          ListTile(
+            leading: const Icon(Icons.home),
+            iconColor: Colors.pink,
+            title: const Text("Configurações"),
+            tileColor: const Color.fromARGB(14, 233, 30, 98),
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ConfiguracoesPage()));
+            },
+          ),
+          const Divider(color: Colors.transparent),
+          ListTile(
+            leading: const Icon(Icons.done_all),
+            iconColor: Colors.pink,
+            title: const Text("Termos de uso e privacidade"),
+            tileColor: const Color.fromARGB(14, 233, 30, 98),
+            onTap: () {
+              showModalBottomSheet(
+                  context: context,
                   builder: (BuildContext bc) {
-                    return  AlertDialog(
-                      icon: const Icon(Icons.sentiment_dissatisfied,size: 45,color: Colors.white),
-                      backgroundColor: Colors.pink,                      
-                      title: const Text("Que Pena!",style: TextStyle(color: Colors.white)),
-                      content: const Wrap(
-                        children: [
-                            Text("Tem certeza que deseja sair?",textAlign: TextAlign.center,style: TextStyle(color: Colors.white,)),
-                        ] 
-                      ), 
-                                      actions: [
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => const LoginPage()),
-                      );
-                    },
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.pink, 
-                    ),
-                    child: const Text("Sim", style: TextStyle(color: Colors.white)),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.grey, // Corrigido para definir a cor de fundo
-                    ),
-                    child: const Text("Não", style: TextStyle(color: Colors.white)),
-                  ),
-                ],
-                                    );
-                                  },
-                                  );
-                                },
-                                
+                    return SingleChildScrollView(
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 20, horizontal: 12),
+                        child: const Center(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text("Termos de Uso e Política de Privacidade",
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.w700)),
+                              Text(
+                                "Ao utilizar nossos serviços, você concorda com os seguintes termos e condições. Coletamos informações pessoais, como nome, e-mail e dados de uso, para proporcionar  uma melhor experiência ao usuário. Seus dados são armazenados de forma segura e utilizados apenas para fins operacionais, aprimoramento dos nossos serviços e comunicação com você. Não compartilhamos suas informações com terceiros sem seu consentimento, exceto quando exigido por lei. Você tem o direito de acessar, corrigir ou solicitar a exclusão de seus dados pessoais a qualquer momento. Utilizamos cookies e tecnologias similares para melhorar a funcionalidade do nosso site e analisar o uso do serviço. Ao continuar a navegação, você aceita o uso dessas tecnologias. Reservamo-nos o direito de modificar estes termos a qualquer momento. Quaisquer alterações serão publicadas em nosso site e entrarão em vigor imediatamente. Recomendamos revisar periodicamente esta política para estar ciente de quaisquer mudanças. Se tiver dúvidas ou preocupações sobre nossa política de privacidade, entre em contato conosco.",
+                                textAlign: TextAlign.justify,
                               ),
                             ],
+                          ),
+                        ),
+                      ),
+                    );
+                  });
+            },
           ),
-        );
-
+          const Divider(color: Colors.transparent),
+          ListTile(
+            leading: const Icon(Icons.exit_to_app),
+            iconColor: Colors.pink,
+            title: const Text("Sair"),
+            tileColor: const Color.fromARGB(14, 233, 30, 98),
+            onTap: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext bc) {
+                  return AlertDialog(
+                    icon: const Icon(Icons.sentiment_dissatisfied,
+                        size: 45, color: Colors.white),
+                    backgroundColor: Colors.pink,
+                    title: const Text("Que Pena!",
+                        style: TextStyle(color: Colors.white)),
+                    content: const Wrap(children: [
+                      Text("Tem certeza que deseja sair?",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Colors.white,
+                          )),
+                    ]),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const LoginPage()),
+                          );
+                        },
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors.pink,
+                        ),
+                        child: const Text("Sim",
+                            style: TextStyle(color: Colors.white)),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        style: TextButton.styleFrom(
+                          backgroundColor: Colors
+                              .grey, // Corrigido para definir a cor de fundo
+                        ),
+                        child: const Text("Não",
+                            style: TextStyle(color: Colors.white)),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import '../componentes/custom_drawer.dart';
 
-
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
@@ -33,6 +32,65 @@ class _MainPageState extends State<MainPage> {
           )),
         ),
         drawer: const CustomDrawer(),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 60),
+                width: double.infinity,
+                child: Card(
+                  elevation: 8,
+                  shadowColor: Colors.grey,
+                  child: Padding(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: Column(
+                      children: [
+                        const Row(
+                          children: [
+                            Text(
+                              "Importancia do calculo de Imc",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(
+                            height: 8), // Espaçamento entre o texto e a imagem
+                        Image.network(
+                          "https://www.plasticadosonho.com.br/wp-content/uploads/2017/12/blog-06-como-calcular-o-imc.jpg",
+                          height:
+                              150, // Você pode ajustar a altura da imagem conforme necessário
+                          width: double.infinity,
+                          fit: BoxFit
+                              .cover, // Ajusta a imagem para cobrir o espaço disponível
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                            "O cálculo do Índice de Massa Corporal (IMC) é crucial para avaliar a saúde de uma pessoa, pois ajuda a determinar se está dentro de uma faixa de peso saudável. Ele fornece uma estimativa da gordura corporal com base no peso e altura, sendo um indicador simples e acessível. O IMC é utilizado por profissionais de saúde para identificar riscos de doenças relacionadas ao sobrepeso, como diabetes e hipertensão. Além disso, serve como uma ferramenta inicial para recomendar mudanças no estilo de vida, dieta e exercícios físicos. Monitorar o IMC ao longo do tempo ajuda a manter um controle sobre a saúde geral e prevenir complicações futuras."),
+                        Container(
+                          width: double.infinity,
+                          alignment: Alignment.centerRight,
+                          child: TextButton(
+                              onPressed: () {},
+                              child: const Text("Ler mais",
+                                  style: TextStyle(
+                                      color: Colors.pink,
+                                      fontWeight: FontWeight.w700))),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
